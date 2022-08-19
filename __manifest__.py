55a67b30e3ab4c7d152198fa43c8128c9fbefa9f
jeffrepo <jeffer.leo93@gmail.com>
2022-08-19 00:07:29 -0600

    formato

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Formato factura fel',
+    'version': '1.0',
+    'category': 'Hidden',
+    'sequence': 6,
+    'summary': 'Formato factura fel',
+    'description': """
+
+""",
+    'depends': ['account'],
+    'data': [
+        'views/reporte_factura.xml',
+        'views/report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
